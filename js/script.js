ec3541baf84608a653e51d68f6f73e709c429b4c
nurulislamrimon <nurulislamrimon@gmail.com>
2022-03-01 18:41:59 +0600

    others info added

--- js/script.js
+++ js/script.js
@@ -14,7 +14,10 @@ const getPhoneData = input => {
 // display phones data
 const displayPhones = phones => {
     const phonesContainer = document.getElementById("phone-display");
+    const detailsContainer = document.getElementById("phone-details");
+    // clearing
     phonesContainer.textContent = "";
+    detailsContainer.textContent = "";
     // no phone found debugger
     if (phones.status == false) {
         const notFound = document.createElement("div");
@@ -29,8 +32,8 @@ const displayPhones = phones => {
             // destructuring
             const { image, brand, phone_name, slug } = phone;
             newPhoneContainer.innerHTML = `
-            <div class="card shadow" style ="width: 18rem" > 
-                <img src="${phone.image}" height="250"class="card-img-top" alt="...">
+            <div class="card shadow" style ="width: 18rem"> 
+                <img src="${phone.image}" width="250" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${phone_name}</h5>
                         <h6 class="card-text">Brand: ${brand}</h6>
@@ -48,19 +51,23 @@ const phoneDetails = id => {
     fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
         .then(res => res.json())
         .then(data => displayPhoneDetails(data));
+    console.log(id);
 };
 // display phone details
 const displayPhoneDetails = fullDetails => {
     const detailsContainer = document.getElementById("phone-details");
     detailsContainer.textContent = "";
+    console.log(fullDetails);
     // destructuring
     const { image, name, releaseDate } = fullDetails.data;
-    const { storage, displaySize, chipSet, memory } = fullDetails.data.mainFeatures;
+    const { storage, displaySize, chipSet, memory, sensors } = fullDetails.data.mainFeatures;
+    // const { Bluetooth, GPS, NFC, Radio, USB, WLAN } = fullDetails?.data?.others;
+    // all sensor in a variable
+    const allSensors = sensors.join(", ");
 
     const newDetailsContainer = document.createElement("div");
     newDetailsContainer.classList.add("details-style");
     newDetailsContainer.innerHTML = `
-        <img src="${image}" height="400">
         <div class="ms-5">
             <h2>Name : ${name}</h2>
             <p>Release-date : ${releaseDate}</p>
@@ -69,7 +76,15 @@ const displayPhoneDetails = fullDetails => {
             <p>Display-Size : ${displaySize}</p>
             <p>Chipset : ${chipSet}</p>
             <p>Memory : ${memory}</p>
-        </div>`;
+            <p>Sensors : ${allSensors}</p>
+            <h6>Others </h6>
+            <p>Bluetooth : ${fullDetails?.data?.others?.Bluetooth}</p>
+            <p>GPS : ${fullDetails?.data?.others?.GPS}</p>
+            <p>NFC : ${fullDetails?.data?.others?.NFC}</p>
+            <p>Radio : ${fullDetails?.data?.others?.Radio}</p>
+            <p>USB : ${fullDetails?.data?.others?.USB}</p>
+            <p>WLAN : ${fullDetails?.data?.others?.WLAN}</p>
+        </div>
+        <img src="${image}" height="500">`;
     detailsContainer.appendChild(newDetailsContainer);
-    console.log(storage, displaySize, chipSet, memory);
 }
